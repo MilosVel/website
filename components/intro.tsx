e00584c275524152e048fcabe0f05c10dc4fd1fd
milos <milosvelickovic1992@gmail.com>
2026-04-11 10:21:38 +0200

    intro section headline

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,29 +21,40 @@ export default function Intro() {
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src={profilePicture}
-              alt="Miloš Veličković portrait"
-              width="500"
-              height="500"
-              quality="100"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
+      <div>
+
+        <motion.h1
+          className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <span className="font-bold">Hello, I'm Miloš.</span>
+          <div>
+            I enjoy
+            building <span className="italic">sites & apps</span>.  </div>
+        </motion.h1>
+
+        <motion.div
+          className="flex items-center justify-center relative"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "tween",
+            duration: 0.2,
+          }}
+        >
+          <Image
+            src={profilePicture}
+            alt="Miloš Veličković portrait"
+            width="500"
+            height="500"
+            quality="100"
+            priority={true}
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+          />
 
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className="absolute bottom-0 left-[50%] text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
@@ -55,18 +66,19 @@ export default function Intro() {
           >
             👋
           </motion.span>
-        </div>
+        </motion.div>
+
       </div>
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+
+      <p
+        className="mb-10 mt-4 px-4 font-medium !leading-[1.5] sm:text-xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Miloš.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> and I enjoy
-        building <span className="italic">sites & apps</span>.
-      </motion.h1>
+
+        Full-Stack Developer (6+ yrs) | React, Next.js, TypeScript, Node.js, Python, FastAPI, PostgreSQL | AI, RAG, LLM APIs | Building production-grade SaaS products
+      </p>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -113,6 +125,6 @@ export default function Intro() {
           <FaGithubSquare />
         </a>
       </motion.div>
-    </section>
+    </section >
   );
 }
